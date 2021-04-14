2af505d82d1bcfb8a0bb20b6f641c428e989f481
Laura Osorio <laurisosorio7@hotmail.com>
2021-04-14 18:44:41 -0500

    Creando página principal

---
+++ app/Views/home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="<?php echo base_url() ?>/public/assets/css/home.css">
+	<title>Alquiler de Inmuebles</title>
+</head>
+
+<body>
+	<header class="header">
+		<h1>Alquila</h1>
+		<ul class="header-menu">
+			<li><button class="header-menu-button" type="button">Iniciar Sesión</button></li>
+			<li><button class="header-menu-button" type="button">Registrarse</button></li>
+		</ul>
+	</header>
+
+	<main class="main">
+		<div class="main-title">
+			<h1>Dejanos encontrar el apartamento de tus sueños </h1>
+			<h3>O convierte en el mejor anfitrión</h3>
+		</div>
+		<div class="main-grid">
+			<img class="imagen-a" src="https://image.freepik.com/foto-gratis/hermosa-pareja-relajante-casa_23-2148813846.jpg" alt="">
+			<img class="imagen-b" src="https://image.freepik.com/foto-gratis/mujer-artista-pinta-cuadro-sobre-lienzo-hace-bocetos-lapiz-sentada-casa-encierro_165285-2397.jpg" alt="">
+			<img class="imagen-c" src="https://image.freepik.com/foto-gratis/pareja-romantica-sofa-casa-jugo_23-2148813910.jpg" alt="">
+			<img class="imagen-d" src="https://image.freepik.com/foto-gratis/mujer-afroamericana-agotada-durmiendo-sofa-casa-dejando-caer-telefono-celular-suelo-fatiga_165285-2410.jpg" alt="">
+			<img class="imagen-e" src="https://image.freepik.com/foto-gratis/pareja-joven-sonriente-cocinar-juntos-casa_23-2148813877.jpg" alt="">
+			<img class="imagen-f" src="https://image.freepik.com/foto-gratis/disparo-vertical-empresario-que-trabaja-casa_273609-20504.jpg" alt="">
+			<img class="imagen-g" src="https://image.freepik.com/foto-gratis/perro-tiro-completo-mirando-apple_23-2148861561.jpg" alt="">
+			<img class="imagen-h" src="https://image.freepik.com/foto-gratis/mujer-sonriente-tarjeta-credito-smartphone-casa_23-2148793435.jpg" alt="">
+			<!-- <img class="imagen-i" src="https://image.freepik.com/foto-gratis/primer-disparo-vertical-perro-salchicha-orejas-largas-aislado-sofa-blanco_181624-29094.jpg" alt="">
+		 -->
+		 <img src="https://image.freepik.com/foto-gratis/foto-vertical-sentada-piso-feliz-madre-cabello-rubio-sosteniendo-su-hija-sonriente-pijama-pie-sobre-alfombra-cerca-cama-gris-jugando-perro-shiba-inu-frente-grandes-ventanas-noche_354437-34.jpg" alt="">
+		</div>
+	</main>
+</body>
+
+</html>
